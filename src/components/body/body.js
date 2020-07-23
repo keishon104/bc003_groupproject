7b3e02f79acd00bab47cb30e65e2c5893d1bcf0f
Keishon <keishon104@gmail.com>
2020-07-23 09:31:53 -0400

    Adds Forms and File Structure

---
+++ src/components/body/body.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {Button, InputGroup, FormControl} from 'react-bootstrap';
+import './body.css';
+
+
+
+class Body extends Component {
+
+
+
+
+  render() {
+    return (
+      <div className="InputFields">
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">T</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="How much should you budget for Toys?"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">S</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="How much should you budget for Toys?"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+
+        <InputGroup className="mb-3">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="basic-addon1">C</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="How much should you budget for Toys?"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </InputGroup>
+
+      </div>
+
+
+  )}
+
+
+}
+
+export default Body;
